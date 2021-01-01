1e6264d2b3b060952d463ac7c8792cb20e96cde8
T-Aruga <baideneo@gmail.com>
2021-01-02 00:58:28 +0900

    Add good and read funcead

--- boardproject/boardapp/urls.py
+++ boardproject/boardapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import signupfunc, loginfunc, listfunc, logoutfunc, detailfunc
+from .views import signupfunc, loginfunc, listfunc, logoutfunc, detailfunc, goodfunc, readfunc
 
 urlpatterns = [
     path('signup/', signupfunc, name='signup'),
@@ -7,4 +7,6 @@ urlpatterns = [
     path('list/', listfunc, name='list'),
     path('logout/', logoutfunc, name='logout'),
     path('detail/<int:pk>', detailfunc, name='detail'),
+    path('good/<int:pk>', goodfunc, name='good'),
+    path('read/<int:pk>', readfunc, name='read'),
 ]
