49b3898d203cdb2ef5ef0096115ead3171ca662d
T-Aruga <baideneo@gmail.com>
2021-01-02 01:10:51 +0900

    Add create func

--- boardproject/boardproject/settings.py
+++ boardproject/boardproject/settings.py
@@ -117,3 +117,20 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 LOGIN_URL = 'login'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
