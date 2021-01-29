435ed8abb186df561a935f174edd3a0f1acfeb5b
T-Aruga <baideneo@gmail.com>
2021-01-30 06:57:19 +0900

    Add admin and index view

--- techpit/finchart/views.py
+++ techpit/finchart/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
+from django.views.generic import TemplateView
+from .models import Fstatement
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'finchart/index.html'
+
+    def get_context_data(self, **kwargs):
+        fstatement_list = Fstatement.objects.all().order_by('company')
+        params = {
+            'fstatement_list': fstatement_list,
+        }
+        return params
